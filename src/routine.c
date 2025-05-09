06f8e3a9f33c79cea1a4169586194dbc78a95ed7
eyadaq <eyadaq@live.com>
2025-05-09 23:40:36 +0300

    Improve print formatting and enhance fork acquisition logging in routine.c

--- src/routine.c
+++ src/routine.c
@@ -6,7 +6,7 @@
 /*   By: eaqrabaw <eaqrabaw@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 07:26:42 by eaqrabaw          #+#    #+#             */
-/*   Updated: 2025/05/09 22:47:30 by eaqrabaw         ###   ########.fr       */
+/*   Updated: 2025/05/09 23:39:32 by eaqrabaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	ft_print(t_philo *philo, char *str)
 {
 	pthread_mutex_lock(&philo->data->print_lock);
-	printf("%ld %d %s\n", (long)(get_time() - philo->data->start_time), 
+	printf("%6.0ld %3.0d %s\n", (long)(get_time() - philo->data->start_time), 
 			philo->id + 1, str);
 	pthread_mutex_unlock(&philo->data->print_lock);
 }
@@ -24,13 +24,17 @@ void	ft_eat(t_philo *philo)
 {
 	if (philo->id % 2 == 0)
 	{
-		pthread_mutex_lock(philo->right_fork);
-		pthread_mutex_lock(philo->left_fork);
+	    pthread_mutex_lock(philo->right_fork);
+	    ft_print(philo, "has taken a fork");
+	    pthread_mutex_lock(philo->left_fork);
+	    ft_print(philo, "has taken a fork");
 	}
 	else
 	{
-		pthread_mutex_lock(philo->left_fork);
-		pthread_mutex_lock(philo->right_fork);
+	    pthread_mutex_lock(philo->left_fork);
+	    ft_print(philo, "has taken a fork");
+	    pthread_mutex_lock(philo->right_fork);
+	    ft_print(philo, "has taken a fork");
 	}
 	pthread_mutex_lock(&philo->data->simulation_lock);
 	philo->last_meal = get_time();
